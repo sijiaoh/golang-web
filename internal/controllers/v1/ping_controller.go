907d754b9cc9c29cb1c75c0c6af4e39768939169
sijiaoh <sijiaoh@outlook.com>
2024-11-30 00:45:09 +0000

    Extract router and controllers from app/main.go

---
+++ internal/controllers/v1/ping_controller.go
@@ -0,0 +1,13 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PingController(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
---
+++ internal/router/setup_router.go
@@ -0,0 +1,10 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	controllers_v1 "github.com/sijiaoh/golang-web/internal/controllers/v1"
+)
+
+func SetupRouter(r *gin.Engine) {
+	r.GET("/ping", controllers_v1.PingController)
+}
